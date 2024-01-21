69884c0647edaf8678e4deb4574b9a6ce9eb3162
saifvandana <vandanasaif@gmail.com>
2024-01-20 20:10:08 -0500

    added layout and navigation

--- components/Nav.js
+++ components/Nav.js
@@ -8,7 +8,7 @@ import {
   HiEnvelope,
 } from 'react-icons/hi2';
 
-// nav data
+//  data
 export const navData = [
   { name: 'home', path: '/', icon: <HiHome /> },
   { name: 'about', path: '/about', icon: <HiUser /> },
@@ -26,8 +26,20 @@ export const navData = [
   },
 ];
 
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 const Nav = () => {
-  return <nav>nav</nav>;
+  const router = useRouter();
+  const pathname = router.pathname;
+  return ( <nav className='flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen bg-red-500/30 p-20'>{
+    /* inner */}
+    <div className='flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 xl:px-0 h-80 xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full'>
+      {navData.map((link, index) => {
+        return <Link href={link.path}>{link.icon}</Link>
+      })}
+    </div>
+    </nav>);
 };
 
 export default Nav;
